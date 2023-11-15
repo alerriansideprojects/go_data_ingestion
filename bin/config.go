6d8bb215f85080834fbe4172826ea9068d8533e6
alerrian <steven.a.anderson2012@gmail.com>
2023-11-14 18:27:04 -0700

    adds support for multiple configurations

--- bin/config.go
+++ bin/config.go
@@ -3,7 +3,9 @@ package config
 import (
 	"fmt"
 	"gopkg.in/yaml.v3"
+	"encoding/json"
 	"os"
+	"log"
 )
 type Config struct {
 	tap Tap
@@ -11,23 +13,21 @@ type Config struct {
 }
 
 type Tap struct {
-	Type string `yaml:"type"`
-	Name string `yaml:"name"`
-	Path string `yaml:"path"`
+	Type string
+	Name string
+	Path string
 }
 
 type Target struct {
-	Type string `yaml:"type"`
-	Name string `yaml:"name"`
+	Type string
+	Name string
 }
 
-func (config *Config) Read(path string) {
+func (config *Config) ReadYaml(path string) {
 	yamlData, err := os.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("YAML Data: ", string(yamlData))
 
 	var cfg Config
 
@@ -42,4 +42,28 @@ func (config *Config) Read(path string) {
 	fmt.Printf("Tap Path: %s\n", cfg.tap.Path)
 	fmt.Printf("Target Type: %s\n", cfg.target.Type)
 	fmt.Printf("Target Name: %s\n", cfg.target.Name)
+	fmt.Printf("Detected YAML config File: %s...\n", path)
+}
+
+func (config *Config) ReadJson(path string) {
+	fmt.Printf("Detected JSON config File: %s...\n", path)
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Unable to read file due to %s\n", err)
+	}
+
+	var cfg Config
+	err = json.Unmarshal(content, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Config Full: %s\n", cfg)
+	fmt.Printf("Tap Type: %s\n", cfg.tap.Type)
+	fmt.Printf("Tap Name: %s\n", cfg.tap.Name)
+	fmt.Printf("Tap Path: %s\n", cfg.tap.Path)
+	fmt.Printf("Target Type: %s\n", cfg.target.Type)
+	fmt.Printf("Target Name: %s\n", cfg.target.Name)
+	fmt.Printf("Detected YAML config File: %s...\n", path)
 }
--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 	config "go_data_ingestion/bin"
 )
 
-var configPath = "./configs/config.yaml"
-
 func main() {
 	fmt.Println("Reading config...")
-	var c = config.Config{}
-	c.Read(configPath)
-}
+	if files, err := os.ReadDir("./configs/"); err != nil {
+		panic(err)
+	} else {
+		for _, file := range files {
+			configPath := "./configs/" + file.Name()
+			var c = config.Config{}
+			switch configPath[len(configPath)-4:] {
+			case "yaml":
+				c.ReadYaml(configPath)
+			case "json":
+				c.ReadJson(configPath)
+			}
+		}
+	
+	}
 
+}
